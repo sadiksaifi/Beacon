83923517b70f2cbc4fc166325037717d3dbead91
Sadik Saifi <mail@sadiksaifi.dev>
2026-02-18 11:01:40 +0530

    feat(L1-S2): set up SwiftData persistence layer
    Add modelContainer for Connection to the app's WindowGroup.

--- Beacon/BeaconApp.swift
+++ Beacon/BeaconApp.swift
@@ -1,3 +1,4 @@
+import SwiftData
 import SwiftUI
 
 @main
@@ -6,5 +7,6 @@ struct BeaconApp: App {
         WindowGroup {
             MainTabView()
         }
+        .modelContainer(for: Connection.self)
     }
 }
